64f2e184902258ef526be378aa7d6c87dbfd7cc7
noreyni03 <onoreyni03@gmail.com>
2025-07-01 22:24:38 +0100

    feat(UI) : Mise en place du frontend

---
+++ backend/src/main/java/com/newsplatform/NewsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.newsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/newsplatform/NewsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
