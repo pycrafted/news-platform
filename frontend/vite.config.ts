c7b4f879c5ce715c661f2b16a4303ee59b293699
noreyni03 <onoreyni03@gmail.com>
2025-07-02 09:29:40 +0100

    feat(UI) : Mise en place du projet

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#eff6ff',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+        }
+      }
+    },
+  },
+  plugins: [
+    forms,
+    typography,
+  ],
+}
