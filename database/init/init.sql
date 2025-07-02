25aa1ea17df861c21be900debd9e17d2e7db1ec9
noreyni03 <onoreyni03@gmail.com>
2025-07-02 12:12:09 +0100

    feat(UI) : Mise en place du projet

--- database/init/init.sql
+++ database/init/init.sql
@@ -1,7 +1,9 @@
 -- Création de la base de données et de l'utilisateur
 CREATE DATABASE newsplatform;
-CREATE USER admin_9F8a7C WITH ENCRYPTED PASSWORD 'G7!pR2@vLq8z';
-GRANT ALL PRIVILEGES ON DATABASE newsplatform TO admin_9F8a7C;
+-- Le mot de passe doit être passé via une variable d'environnement
+-- Exemple : export DB_PASSWORD=... puis utiliser dans un script d'automatisation
+-- CREATE USER admin_9F8a7C WITH ENCRYPTED PASSWORD '<mot_de_passe_depuis_env>';
+-- GRANT ALL PRIVILEGES ON DATABASE newsplatform TO admin_9F8a7C;
 
 -- Extensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
